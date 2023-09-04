210a6656d5d1ed75ca303671dc5ef7cbe899ba58
Asees <assiesq23@gmil.com>
2023-09-04 19:14:48 +0500

    product and cat add

--- Estore/models.py
+++ Estore/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class category(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+class product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField()
+    stock = models.IntegerField()
+    price = models.IntegerField()
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
